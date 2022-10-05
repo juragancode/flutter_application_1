c12653150102d580eb0cbab6ab8d7be2f0a5c7a8
xaiur <davinoverclock@gmail.com>
2022-10-05 14:37:41 +0800

    Visible dan Invisible Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,15 +16,26 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("My App"),
         ),
-        body: Center(
-          child: Text(
-            "Hello World",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontFamily: "Roboto",
-              fontSize: 30,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 250,
+              height: 50,
+              color: Colors.orange,
             ),
-          ),
+            Container(
+              width: 300,
+              height: 50,
+              color: Colors.lime,
+            ),
+            Container(
+              width: 350,
+              height: 50,
+              color: Colors.teal,
+            ),
+          ],
         ),
       ),
     );
