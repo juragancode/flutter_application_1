9f4a11c11efdf9b58661444d875c5652a54e1e8c
xaiur <davinoverclock@gmail.com>
2022-10-06 12:39:55 +0800

    3. ListTile

--- lib/main.dart
+++ lib/main.dart
@@ -23,23 +23,103 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("My App"),
+          centerTitle: true,
+          title: Text("List Tile"),
         ),
-        body: ListView.separated(
-          separatorBuilder: (context, index) {
-            return Divider();
-          },
-          itemCount: myColors.length,
-          itemBuilder: (context, index) {
-            return Container(
-              width: 400,
-              height: 400,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: myColors[index],
+        body: ListView(
+          children: [
+            ListTile(
+              title: Text("Hermawan Rinnaldi"),
+              subtitle: Text(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis,
               ),
-            );
-          },
+              leading: CircleAvatar(),
+              trailing: Text("12:30 PM"),
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            ListTile(
+              title: Text("Hermawan Rinnaldi"),
+              subtitle: Text("This is subtitle"),
+              leading: CircleAvatar(),
+              trailing: Text("12:30 PM"),
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            ListTile(
+              title: Text("Hermawan Rinnaldi"),
+              subtitle: Text("This is subtitle"),
+              leading: CircleAvatar(),
+              trailing: Text("12:30 PM"),
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            ListTile(
+              title: Text("Hermawan Rinnaldi"),
+              subtitle: Text("This is subtitle"),
+              leading: CircleAvatar(),
+              trailing: Text("12:30 PM"),
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            ListTile(
+              title: Text("Hermawan Rinnaldi"),
+              subtitle: Text("This is subtitle"),
+              leading: CircleAvatar(),
+              trailing: Text("12:30 PM"),
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            ListTile(
+              title: Text("Hermawan Rinnaldi"),
+              subtitle: Text("This is subtitle"),
+              leading: CircleAvatar(),
+              trailing: Text("12:30 PM"),
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            ListTile(
+              title: Text("Hermawan Rinnaldi"),
+              subtitle: Text("This is subtitle"),
+              leading: CircleAvatar(),
+              trailing: Text("12:30 PM"),
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            ListTile(
+              title: Text("Hermawan Rinnaldi"),
+              subtitle: Text("This is subtitle"),
+              leading: CircleAvatar(),
+              trailing: Text("12:30 PM"),
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            ListTile(
+              title: Text("Hermawan Rinnaldi"),
+              subtitle: Text("This is subtitle"),
+              leading: CircleAvatar(),
+              trailing: Text("12:30 PM"),
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            ListTile(
+              title: Text("Hermawan Rinnaldi"),
+              subtitle: Text("This is subtitle"),
+              leading: CircleAvatar(),
+              trailing: Text("12:30 PM"),
+            ),
+          ],
         ),
       ),
     );
