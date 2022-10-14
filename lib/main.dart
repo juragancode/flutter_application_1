897b67a75b4a9a156c37a282bf895273338986d8
xaiur <davinoverclock@gmail.com>
2022-10-14 22:56:53 +0800

    Flutter - ListView

--- lib/main.dart
+++ lib/main.dart
@@ -15,70 +15,336 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           centerTitle: true,
-          title: Text("Image Widget"),
+          title: Text("ListView"),
         ),
-        body: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            children: [
-              Container(
-                width: 100,
-                height: 35,
-                color: Colors.greenAccent,
-                child: Center(
-                  child: Text("Test"),
-                ),
+        body: ListView(
+          /// Ukuran ListView mengikuti scrollDirection, apabila scrollDirection: Axis.horizontal maka yang bisa diubah hanya width.
+
+          scrollDirection: Axis.vertical,
+
+          /// [scrollDirection] tidak berlaku untuk Stack, hanya berlaku untuk Column dan Row
+          children: [
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.greenAccent,
+              child: Center(
+                child: Text("Test"),
               ),
-              Container(
-                width: 100,
-                height: 35,
-                color: Colors.greenAccent,
-                child: Center(
-                  child: Text("Test"),
-                ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.white,
+              child: Center(
+                child: Text("Test"),
               ),
-              Container(
-                width: 100,
-                height: 35,
-                color: Colors.greenAccent,
-                child: Center(
-                  child: Text("Test"),
-                ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.greenAccent,
+              child: Center(
+                child: Text("Test"),
               ),
-              Container(
-                width: 100,
-                height: 35,
-                color: Colors.greenAccent,
-                child: Center(
-                  child: Text("Test"),
-                ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.white,
+              child: Center(
+                child: Text("Test"),
               ),
-              Container(
-                width: 100,
-                height: 35,
-                color: Colors.greenAccent,
-                child: Center(
-                  child: Text("Test"),
-                ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.greenAccent,
+              child: Center(
+                child: Text("Test"),
               ),
-              Container(
-                width: 100,
-                height: 35,
-                color: Colors.greenAccent,
-                child: Center(
-                  child: Text("Test"),
-                ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.white,
+              child: Center(
+                child: Text("Test"),
               ),
-              Container(
-                width: 100,
-                height: 35,
-                color: Colors.greenAccent,
-                child: Center(
-                  child: Text("Test"),
-                ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.greenAccent,
+              child: Center(
+                child: Text("Test"),
               ),
-            ],
-          ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.white,
+              child: Center(
+                child: Text("Test"),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.greenAccent,
+              child: Center(
+                child: Text("Test"),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.white,
+              child: Center(
+                child: Text("Test"),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.greenAccent,
+              child: Center(
+                child: Text("Test"),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.white,
+              child: Center(
+                child: Text("Test"),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.greenAccent,
+              child: Center(
+                child: Text("Test"),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.white,
+              child: Center(
+                child: Text("Test"),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.greenAccent,
+              child: Center(
+                child: Text("Test"),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.white,
+              child: Center(
+                child: Text("Test"),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.greenAccent,
+              child: Center(
+                child: Text("Test"),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.white,
+              child: Center(
+                child: Text("Test"),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.greenAccent,
+              child: Center(
+                child: Text("Test"),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.white,
+              child: Center(
+                child: Text("Test"),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.greenAccent,
+              child: Center(
+                child: Text("Test"),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.white,
+              child: Center(
+                child: Text("Test"),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.greenAccent,
+              child: Center(
+                child: Text("Test"),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.white,
+              child: Center(
+                child: Text("Test"),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.greenAccent,
+              child: Center(
+                child: Text("Test"),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.white,
+              child: Center(
+                child: Text("Test"),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.greenAccent,
+              child: Center(
+                child: Text("Test"),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.white,
+              child: Center(
+                child: Text("Test"),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.greenAccent,
+              child: Center(
+                child: Text("Test"),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.white,
+              child: Center(
+                child: Text("Test"),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.greenAccent,
+              child: Center(
+                child: Text("Test"),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.white,
+              child: Center(
+                child: Text("Test"),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.greenAccent,
+              child: Center(
+                child: Text("Test"),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.white,
+              child: Center(
+                child: Text("Test"),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.greenAccent,
+              child: Center(
+                child: Text("Test"),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.white,
+              child: Center(
+                child: Text("Test"),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.greenAccent,
+              child: Center(
+                child: Text("Test"),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.white,
+              child: Center(
+                child: Text("Test"),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.greenAccent,
+              child: Center(
+                child: Text("Test"),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 35,
+              color: Colors.white,
+              child: Center(
+                child: Text("Test"),
+              ),
+            ),
+          ],
         ),
       ),
     );
