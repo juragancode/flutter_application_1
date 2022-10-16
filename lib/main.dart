47ef08add81874e7cad21abac28b28e7e07684df
xaiur <davinoverclock@gmail.com>
2022-10-16 14:29:12 +0800

    Flutter - Mapping Collection 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -9,6 +10,19 @@ main() {
 }
 
 class MyApp extends StatelessWidget {
+  List<KotakWarna> allItems = List.generate(
+    30,
+    (index) => KotakWarna(
+      text: "Kotak no ${index + 1}",
+      warna: Color.fromARGB(
+        255,
+        190 + Random().nextInt(256),
+        190 + Random().nextInt(256),
+        190 + Random().nextInt(256),
+      ),
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,21 +30,12 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           centerTitle: true,
-          title: Text("Extract Widget"),
+          title: Text("Mapping Collection"),
         ),
         body: SingleChildScrollView(
           scrollDirection: Axis.vertical,
           child: Column(
-            children: [
-              KotakWarna(text: "Hijau", warna: Colors.green),
-              KotakWarna(text: "Biru", warna: Colors.blueAccent.shade400),
-              KotakWarna(text: "Pink", warna: Colors.pink.shade700),
-              KotakWarna(text: "Orange", warna: Colors.orange.shade800),
-              KotakWarna(text: "Ungu", warna: Colors.purpleAccent.shade700),
-              KotakWarna(text: "Biru", warna: Colors.blueAccent.shade400),
-              KotakWarna(text: "Pink", warna: Colors.pink.shade700),
-              KotakWarna(text: "Orange", warna: Colors.orange.shade800),
-            ],
+            children: allItems,
           ),
         ),
       ),
